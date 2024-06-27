ae40b16b0d4e40d471ffddb5ea13549711fedfea
mawojcik <wojcik.maciej.k@gmail.com>
2024-06-27 20:46:24 +0200

    changed to sorting in-place and printing station data with each api request

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ if __name__ == "__main__":
     print("Fetching data from API, please wait")
     handler = ApiHandler()
     stations = handler.get_all_stations()
-    stations = sorted(stations, key=lambda station: station.id)
+    stations.sort(key=lambda station: station.id)
 
     for station in stations:
         station.installations = handler.get_installations_of_station(station.id)
-
-    for station in stations:
         print(station, end="\n\n")
