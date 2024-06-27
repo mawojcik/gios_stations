3b1cadab27870b35aa6655d321c522941e468c3a
mawojcik <wojcik.maciej.k@gmail.com>
2024-06-27 20:43:56 +0200

    using raise_for_status() for handling response status and custom exception for errors

--- api_handler.py
+++ api_handler.py
@@ -3,7 +3,13 @@ import requests
 from models import Station, Installation
 from typing import List
 
-BASEURL = "https://api.gios.gov.pl/pjp-api/rest/station/"
+BASEURL = "https://api.gios.gov.pl/pjp-api/rest/station"
+
+
+class UnsuccessfulRequestException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
 
 
 class ApiHandler:
@@ -12,6 +18,7 @@ class ApiHandler:
     Attributes:
         base_url (str): prefix of URL to get all the stations/installations
     """
+
     def __init__(self, base_url: str = BASEURL):
         self.base_url = base_url
 
@@ -21,27 +28,18 @@ class ApiHandler:
         :return: A List of all stations.
         """
         try:
-            response = requests.get(self.base_url + "findAll")
-            if response.status_code == 200:
-                data = response.json()
-                stations_list = [
-                    Station(station.get("id"), station.get("stationName"))
-                    for station in data
-                    if "id" in station and "stationName" in station
-                ]
-                return stations_list
-
-            elif response.status_code == 429:
-                print("Too many requests for all stations!")
-
-            else:
-                print(f"Failed to retrieve stations data. Status code: {response.status_code}")
-
+            response = requests.get(f"{self.base_url}/findAll")
+            response.raise_for_status()
+            response_json = response.json()
         except requests.exceptions.RequestException as e:
-            print(f"Error requesting data: {e}")
-        except requests.exceptions.JSONDecodeError as e:
-            print(f"Error decoding JSON: {e}")
-        sys.exit(1)
+            raise UnsuccessfulRequestException(f"Error requesting data: {e}") from None
+
+        stations_list = [
+            Station(station.get("id"), station.get("stationName"))
+            for station in response_json
+            if "id" in station and "stationName" in station
+        ]
+        return stations_list
 
     def get_installations_of_station(self, station_id: int) -> List[Installation]:
         """
@@ -50,26 +48,19 @@ class ApiHandler:
         :return: List of all installations for specific station.
         """
         try:
-            response = requests.get(f"{self.base_url}sensors/{station_id}")
-            if response.status_code == 200:
-                data = response.json()
-                installations_list = [
-                    Installation(
-                        installation.get("id"),
-                        installation.get("param", {}).get("paramCode")
-                    ) for installation in data
-                    if "id" in installation and "param" in installation and "paramCode" in installation.get("param", {})
-                ]
-                return installations_list
-
-            elif response.status_code == 429:
-                print("Too many requests for installations!")
-
-            else:
-                print(f"Failed to retrieve installations data for station: #{station_id}. "
-                      f"Status code: {response.status_code}")
-
+            response = requests.get(f"{self.base_url}/sensors/{station_id}")
+            response.raise_for_status()
+            response_json = response.json()
         except requests.exceptions.RequestException as e:
-            print(f"Error requesting data: {e}")
-        except requests.exceptions.JSONDecodeError as e:
-            print(f"Error decoding JSON: {e}")
+            raise UnsuccessfulRequestException(f"Error requesting data: {e}") from None
+
+        installations_list = [
+            Installation(
+                installation.get("id"), installation.get("param", {}).get("paramCode")
+            )
+            for installation in response_json
+            if "id" in installation
+            and "param" in installation
+            and "paramCode" in installation.get("param", {})
+        ]
+        return installations_list
